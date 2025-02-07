766d3a12541057b00974b40c11f38f0770fc362a
Froliano <froliano.yt@gmail.com>
2025-02-07 16:00:43 +0100

    <feat> ajout d'une interface graphique

--- Main.java
+++ Main.java
@@ -1,3 +1,8 @@
+import appareils.Appareil;
+import appareils.Telephone;
+import appareils.ordinateur.CarteGraphique;
+import appareils.ordinateur.OrdinateurModifiable;
+import appareils.ordinateur.Processeur;
 import factory.Usine;
 import inventory.Inventaire;
 
@@ -6,22 +11,17 @@ import java.util.Scanner;
 public class Main
 {
 
-    Scanner scan = new Scanner(System.in);
+    static Scanner scan = new Scanner(System.in);
 
     public static void main(String[] args)
     {
        Usine usine = new Usine();
 
-       usine.creerAppareil(1);
-       usine.creerAppareil(2);
-       usine.creerAppareil(3);
-       usine.creerAppareil(4);
-
-       Inventaire.getInstance().afficherAppareils();
+       faireLeChoix(usine);
 
     }
 
-    private void faireLeChoix(Usine usine)
+    private static void faireLeChoix(Usine usine)
     {
         int choix = 0;
         while (choix != 9)
@@ -62,11 +62,145 @@ public class Main
         }
     }
 
-    private void agirSurAppareil()
+    private static void agirSurAppareil()
     {
+        int choix = -1;
+        Appareil appareil;
+
         System.out.println("quel est l'index de l'element sur lequel tu veux agir");
-        System.out.println("0) quitter");
         choix = scan.nextInt();
         scan.nextLine();
+
+        appareil = Inventaire.getInstance().getAppareil(choix);
+
+        while (choix != 9)
+        {
+            System.out.println("1) activer");
+            System.out.println("2) afficher la description");
+            System.out.println("3) afficher le prix");
+            if (appareil instanceof Telephone)
+            {
+                System.out.println("4) ajouter un contact");
+                System.out.println("5) afficher tout les contacts");
+                System.out.println("6) appeler un contact");
+            } else if (appareil instanceof OrdinateurModifiable)
+            {
+                System.out.println("4) changer la carte graphique");
+                System.out.println("5) changer le processeur");
+            }
+            System.out.println("9) quitter");
+            choix = scan.nextInt();
+            scan.nextLine();
+
+            switch (choix)
+            {
+                case 1:
+                    appareil.activer();
+                    break;
+                case 2:
+                    appareil.getDescription();
+                    break;
+                case 3:
+                    appareil.getPrixFinal();
+                    break;
+                case 4:
+                    if (appareil instanceof Telephone)
+                    {
+                        ajouterContact(((Telephone) appareil));
+                    } else if (appareil instanceof OrdinateurModifiable)
+                    {
+                        changerCarteGraphique(((OrdinateurModifiable) appareil));
+                    }
+                    break;
+
+                case 5:
+                    if (appareil instanceof Telephone)
+                    {
+                        ((Telephone) appareil).afficherContacts();
+                    } else if (appareil instanceof OrdinateurModifiable)
+                    {
+                        changerProcesseur(((OrdinateurModifiable) appareil));
+                    }
+                    break;
+
+                case 6:
+                    if (appareil instanceof Telephone)
+                    {
+                        appellerContact(((Telephone) appareil));
+                    }
+                    break;
+
+                case 9:
+                    break;
+
+                default:
+                    System.out.println("entree invalide");
+            }
+        }
+    }
+
+    private static void appellerContact(Telephone appareil)
+    {
+        System.out.println("Quel est le contact ?");
+        int index = scan.nextInt();
+        scan.nextLine();
+        appareil.appel(index);
+    }
+
+    private static void ajouterContact(Telephone appareil)
+    {
+        System.out.println("Quel est le numero (ex:0610101010) ?");
+        String numero = scan.nextLine();
+        appareil.ajouterContact(numero);
+    }
+
+
+    private static void changerProcesseur(OrdinateurModifiable appareil)
+    {
+        int index = 1;
+        for (Processeur processeur : Processeur.values()) {
+            System.out.println(index + ") " + processeur);
+            index += 1;
+        }
+
+        System.out.println("Quel est le processeur ?");
+        int id = scan.nextInt();
+        scan.nextLine();
+        switch (id) {
+            case 1:
+                appareil.setProcesseur(Processeur.I3_13100);
+            case 2:
+                appareil.setProcesseur(Processeur.I5_13400);
+            case 3:
+                appareil.setProcesseur(Processeur.I7_13700);
+            case 4:
+                appareil.setProcesseur(Processeur.I9_13900);
+            default:
+                return;
+        }
+    }
+
+    private static void changerCarteGraphique(OrdinateurModifiable appareil) {
+        int index = 1;
+        for (CarteGraphique carteGraphique : CarteGraphique.values()) {
+            System.out.println(index + ") " + carteGraphique);
+            index += 1;
+        }
+
+        System.out.println("Quel est la carte graphique ?");
+        int id = scan.nextInt();
+        scan.nextLine();
+        switch (id) {
+            case 1:
+                appareil.setCarteGraphique(CarteGraphique.RTX4060);
+            case 2:
+                appareil.setCarteGraphique(CarteGraphique.RTX4070);
+            case 3:
+                appareil.setCarteGraphique(CarteGraphique.RTX4080);
+            case 4:
+                appareil.setCarteGraphique(CarteGraphique.RTX4090);
+            default:
+                return;
+        }
     }
 }
